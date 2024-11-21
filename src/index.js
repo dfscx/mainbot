f47ec494bd2e969a992a3676117ee4ebce154136
Davi <daviclfs@gmail.com>
2024-11-21 02:37:57 -0300

    Events and commands handler done

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
-const { Client, GatewayIntentBits, Partials } = require('discord.js');
+const { Client, GatewayIntentBits, Partials, Collection } = require('discord.js');
 const { token } = require('./botconfig.json');
+
+const { getEvents } = require('./handlers/eventHandler');
+const { getCommands } = require('./handlers/commandHandler');
 
 const client = new Client({
     intents: [Object.keys(GatewayIntentBits)],
     partials: [Object.keys(Partials)]
 });
 
-client.login(token);
+client.commands = new Collection();
+
+client.login(token).then(() => {
+    getEvents(client);
+    getCommands(client);
+});
